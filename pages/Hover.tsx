d068b225808226a7387ff9bad0591b10422f2f7a
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-06-02 01:23:23 +0530

    test commit

--- pages/Hover.tsx
+++ pages/Hover.tsx
@@ -3,11 +3,11 @@ import { HoverProps } from './Datatypes';
 
 const Hover:React.FC<HoverProps>=({dist})=>{
   return (
-    <div style={{width:'120px',position:'fixed',top:'20px',left:'260px',backgroundColor:'#fafafa',paddingLeft:'10px',paddingRight:'10px',boxShadow:'0px 1px 1px #ebe8e8'}}>
-        <p style={{fontSize:'15px',marginBottom:0,marginTop:'2px'}}>{dist[0]}</p>
+    <div style={{width:'120px',position:'fixed',top:'10px',left:'10px',backgroundColor:'#fafafa',paddingLeft:'10px',paddingRight:'10px',boxShadow:'0px 1px 1px #ebe8e8'}}>
+        <p style={{fontSize:'13px',marginBottom:0,marginTop:'2px'}}>{dist[0]}</p>
         <div style={{display:'flex',justifyContent:'space-between'}}>
-            <p style={{fontSize:'14px',margin:'0'}}>{dist[1]}</p>
-            <p style={{fontSize:'14px',margin:'0'}}>%</p>
+            <p style={{fontSize:'13px',margin:'0'}}>{dist[1]}</p>
+            <p style={{fontSize:'13px',margin:'0'}}>%</p>
         </div>
     </div>
   )
