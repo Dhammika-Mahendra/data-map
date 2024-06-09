e6cd7e3f7b3403dd5805ce1223778d1ed9adafd4
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-06-09 19:54:14 +0530

    Add help image

--- pages/Help.tsx
+++ pages/Help.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { IoCloseSharp } from "react-icons/io5";
 import { helpProps } from './Datatypes';
+import GeogridSL_help from '../public/GeogridSL_help.png'
 
 const Help:React.FC<helpProps>=({setHelp})=>{
   return (
     <div className='w-[90%] h-[90%] fixed top-[5%] left-[5%] bg-white flex flex-col z-20 border border-gray-200 rounded-[5px] shadow-md'>
-        <div className='h-[40px] w-[100%] flex justify-end py-[3px] px-[5px] '>
+        <div className='h-[20px] w-[100%] flex justify-end py-[3px] px-[5px] '>
             <IoCloseSharp className='cursor-pointer' onClick={()=>setHelp(false)}></IoCloseSharp>
+        </div>
+        <div style={{width:'100%',height:'95%',backgroundImage:'url(\'./GeogridSL_help.png\')',backgroundRepeat:'no-repeat',backgroundSize:'contain',backgroundPosition:'center'}}>
+
         </div>
     </div>
   )
